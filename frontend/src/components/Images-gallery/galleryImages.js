1829b90db15a503d81bae28ed8855c61c2ba7c44
AleynaOzpak <aleyna.ozpak@ug.bilkent.edu.tr>
2024-02-27 13:44:23 +0300

    gallery  images  implemented

--- frontend/src/components/Images-gallery/galleryImages.js
+++ frontend/src/components/Images-gallery/galleryImages.js
@@ -5,10 +5,11 @@ import galleryImg04 from '../../assets/images/gallery-04.jpg';
 import galleryImg05 from '../../assets/images/gallery-05.jpg';
 import galleryImg06 from '../../assets/images/gallery-06.jpg';
 import galleryImg07 from '../../assets/images/gallery-07.jpg';
+import galleryImg08 from '../../assets/images/gallery-08.jpg';
 
 const galleryImages = [
     galleryImg01,
-    galleryImg07,
+    galleryImg08,
     galleryImg02,
     galleryImg06,
     galleryImg03,
