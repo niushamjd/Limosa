83aad4dac00bf34c2153227a5783ae447a4bd17f
edaglaroglu <79571668+edaglaroglu@users.noreply.github.com>
2024-04-22 22:58:27 +0300

    Accordion added.

--- frontend/src/pages/Itinerary.jsx
+++ frontend/src/pages/Itinerary.jsx
@@ -3,6 +3,12 @@ import { useLocation } from 'react-router-dom';
 import { Button } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { BASE_URL } from "../utils/config";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Typography from '@mui/material/Typography';
+
 
 function Itinerary() {
   const location = useLocation();
@@ -23,7 +29,7 @@ function Itinerary() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
+        body: JSON.stringify({  
           itineraryId: itinerary._id,
           date,
           period,
@@ -40,34 +46,44 @@ function Itinerary() {
 
   return (
     <div>
-      <h2>Your Itinerary</h2>
-      {Object.entries(itinerary.itineraryEvents).map(([date, periods]) => (
-        <div key={date}>
-          <h3>{date}</h3>
-          {Object.entries(periods).map(([period, activities]) => (
-            <div key={period}>
-              <h4>{period.charAt(0).toUpperCase() + period.slice(1)}</h4>
-              <ul>
-                {activities.map((activity, index) => (
-                  <li key={index}>
-                    <strong>{activity.type === "Place" ? "Visit" : "Eat at"}:</strong>
-                    {activity.name} - <em>{activity.activity}</em>
-                    <Button
-                      size="small"
-                      onClick={() => handleDelete(date, period, activity.name)}
-                      startIcon={<DeleteIcon />}
-                      style={{ marginLeft: '10px' }}
-                    >
-                      Delete
-                    </Button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      ))}
-    </div>
+    <h2>Your Itinerary</h2>
+    {Object.keys(itinerary.itineraryEvents).length > 0 && (
+      <div className="itinerary-accordion">
+        {Object.entries(itinerary.itineraryEvents).map(([date, periods]) => (
+          <Accordion key={date}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>{date}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              {Object.entries(periods).map(([period, activities]) => (
+                <div key={period}>
+                  <Typography variant="h6" component="h2">{`${period.charAt(0).toUpperCase() + period.slice(1)}`}</Typography>
+                  <ul>
+                    {activities.map((activity, index) => (
+                      <li key={index}>
+                        <strong>
+                          {activity.type === "Place" ? "Visit" : "Eat at"}:
+                        </strong>{" "}
+                        {activity.name} - <em>{activity.activity}</em>
+                        <Button
+                          size="small"
+                          onClick={() => handleDelete(date, period, activity.name)}
+                          startIcon={<DeleteIcon />}
+                          style={{ marginLeft: '10px' }}
+                        >
+                          Delete
+                        </Button>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </div>
+    )}
+  </div>
   );
 }
 
