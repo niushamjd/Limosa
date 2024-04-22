5823435c8b16e6a776cfc02e1943cd85838dda3b
denizyyuksel <denizyyuksel@gmail.com>
2024-04-22 14:07:49 +0300

    deletation of events added, backand structured according to

--- backend/routes/itineraries.js
+++ backend/routes/itineraries.js
@@ -2,20 +2,22 @@ import express from "express";
 import {
   createItinerary,
   getItineraryById,
-  getUSerItineraries
+  getUSerItineraries,
+  deleteEventFromItinerary  // Import the new function
 } from "../controllers/itineraryController.js";
-import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 // Create new itinerary
-router.post("/",createItinerary);
+router.post("/", createItinerary);
 // Get single itinerary
 router.get("/:id", getItineraryById);
 // Get all itineraries of a user
 router.get("/user/:userId", getUSerItineraries);
-
-
-
+// Delete an event from an itinerary
+router.post("/delete-event", (req, res) => {
+  console.log("Delete event route hit");
+  deleteEventFromItinerary(req, res);
+});
 
 export default router;
